d2d9c6c016cc5eab77ccaa443dc87079c153afa5
Keiran Collins <keiran@darkforestgroup.com>
2012-03-21 21:18:44 +0000

    Added zeusc properties for build, should now compile CM9

--- device_zeusc.mk
+++ device_zeusc.mk
@@ -44,4 +44,23 @@ PRODUCT_LOCALES += hdpi
 -include device/semc/msm7x30-common/prebuilt/resources-hdpi.mk
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=240
+    ro.telephony.default_network=4 \
+    ro.sf.lcd_density=240 \
+    persist.rild.nitz_plmn= \
+    persist.rild.nitz_long_ons_0= \
+    persist.rild.nitz_long_ons_1= \
+    persist.rild.nitz_long_ons_2= \
+    persist.rild.nitz_long_ons_3= \
+    persist.rild.nitz_short_ons_0= \
+    persist.rild.nitz_short_ons_1= \
+    persist.rild.nitz_short_ons_2= \
+    persist.rild.nitz_short_ons_3= \
+    ro.telephony.ril.v3=datacall \
+    ro.cdma.homesystem=64,65,76,77,78,79,80,81,82,83 \
+    ro.cdma.home.operator.numeric=310012 \
+    ro.cdma.home.operator.alpha=Verizon \
+    ro.cdma.data_retry_config=max_retries=infinite,0,0,60000,120000,480000,900000 \
+    persist.telephony.support_ipv6=true \
+    persist.telephony.support_ipv4=true \
+    ro.ril.vzw.feature=1 \
+    ro.ril.wp.feature=1
